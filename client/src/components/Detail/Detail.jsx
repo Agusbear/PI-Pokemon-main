75b13df676ebb4e4a822459709763b7529c8af82
Agusbear <agustinscaffidi@gmail.com>
2023-02-02 03:34:11 -0300

    Add loader to detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -5,7 +5,7 @@ import * as actions from "../../redux/actions/actions";
 import "./Detail.css";
 
 export default function Detail() {
-  const store = useSelector((state) => state.pokemonDetail);
+  const detail = useSelector((state) => state.pokemonDetail);
   const dispatch = useDispatch();
   const { name } = useParams();
   const [loading, setLoading] = useState(true);
@@ -24,31 +24,41 @@ export default function Detail() {
   return (
     <div>
       <div className="wrapper">
-        <div className="imageContainer">
-          {shiny ? (
-            <img
-              src={store.shinyImage}
-              alt={store.name}
-              className="pokemonImage"
-            />
-          ) : (
-            <img src={store.image} alt={store.name} className="pokemonImage" />
-          )}
-          {store.shinyImage && (
-            <img
-              onClick={shinyHandler}
-              alt="shinyToggle"
-              className="shinyToggleDetail"
-              src="https://lh3.googleusercontent.com/AJm968KMXoYHPYBm6ebdb45wNbtVBhg5X25ZR4PHkZR-oE_rUw8qXDiIN5MThQjzY_6ipGX-1IyJ5c4mBqMT_bFi9O3Wkd2_XcUfLEk3O2qO"
-            />
-          )}
-          <b>{store?.name?.charAt(0).toUpperCase() + store?.name?.slice(1)}</b>
-        </div>
+        {loading ? (
+          <div className="loader"></div>
+        ) : (
+          <div className="imageContainer">
+            {shiny ? (
+              <img
+                src={detail.shinyImage}
+                alt={detail.name}
+                className="pokemonImage"
+              />
+            ) : (
+              <img
+                src={detail.image}
+                alt={detail.name}
+                className="pokemonImage"
+              />
+            )}
+            {detail.shinyImage && (
+              <img
+                onClick={shinyHandler}
+                alt="shinyToggle"
+                className="shinyToggleDetail"
+                src="https://lh3.googleusercontent.com/AJm968KMXoYHPYBm6ebdb45wNbtVBhg5X25ZR4PHkZR-oE_rUw8qXDiIN5MThQjzY_6ipGX-1IyJ5c4mBqMT_bFi9O3Wkd2_XcUfLEk3O2qO"
+              />
+            )}
+            <b>
+              {detail?.name?.charAt(0).toUpperCase() + detail?.name?.slice(1)}
+            </b>
+          </div>
+        )}
         <div className="detailContainer">
           <div className="detailCard">
             <h3 className="cardTitle">Tipos</h3>
             <ul className="typeList">
-              {store.types?.map((type) => {
+              {detail.types?.map((type) => {
                 return (
                   <li key={type.type.name}>
                     {type.type.name[0].toUpperCase() + type.type.name.slice(1)}
@@ -59,12 +69,12 @@ export default function Detail() {
           </div>
           <div className="detailCard">
             <h3 className="cardTitle">ID</h3>
-            <p>{store?.id}</p>
+            <p>{detail?.id}</p>
           </div>
           <div className="detailCard">
             <h3 className="cardTitle">Estadisticas</h3>
             <ul className="typeList">
-              {store.stats?.map((stat) => {
+              {detail.stats?.map((stat) => {
                 return (
                   <li key={stat.stat.name}>
                     {stat.stat.name[0].toUpperCase() + stat.stat.name.slice(1)}:{" "}
@@ -76,8 +86,8 @@ export default function Detail() {
           </div>
           <div className="detailCardLast">
             <h3 className="cardTitle">Extras</h3>
-            <p>Altura: {store?.height}</p>
-            <p>Peso: {store?.weight}</p>
+            <p>Altura: {detail?.height}</p>
+            <p>Peso: {detail?.weight}</p>
           </div>
         </div>
       </div>
